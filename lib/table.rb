2c9d5e11c5ee716835699ffb286f00755f276dbc
David <tennisgeek56@gmail.com>
2019-03-16 12:56:57 -0400

    first commit

---
+++ lib/table.rb
@@ -0,0 +1,54 @@
+#Initialize the table, and places the robot onto the page.
+
+# (col, row) ====> (1, 2) wll be at the spot marked R
+
+# PLACE 1,2,EAST MOVE MOVE LEFT MOVE REPORT Output: 3,3,NORTH 
+#The end is marked with an E
+
+
+          
+#    <--- col ---->
+
+# 4  x  x  x  x  x
+# 3  x  x  x  E  x
+# 2  x  R  x  x  x
+# 1  x  x  x  x  x
+# 0  x  x  x  x  x
+#    0  1  2  3  4
+
+
+class Table
+  attr_accessor :table
+  def initialize
+    @table = Array.new(5) { Array.new(5, 'x') }
+  end
+
+  #user inputs (col, row) --> 
+  def place_piece(pos)
+    table_col = pos[0]
+    table_row = pos[1]
+    @table[table_row][table_col] = 'R'
+
+  end
+
+  def remove_piece(pos)
+    table_col = pos[0]
+    table_row = pos[1]
+    @table[table_row][table_col] = 'x'
+  end
+
+
+
+  #positions will be given as (row, col) 
+  def switch_row_col(pos)
+    pos[0], pos[1] = pos[1], pos[0]
+  end
+  
+end 
+
+
+t = Table.new
+t.place_piece(([1, 2]))
+t.remove_piece([1, 2])
+p t.table
+
